3872167cdfbd205d1ec7e603292d1226245fe15a
BryanPeens <peensbryan1@gmail.com>
2020-07-23 21:55:30 +0200

    created user and task models

--- app/models/task.rb
+++ app/models/task.rb
@@ -14,16 +14,23 @@
 # # - Started at
 # # - Completed at
 
-# class Task < ApplicationRecord    
-#     validates :code, presence: true
-#     validates :customer_id, presence: true    
-#     validates :requested_by, presence: true   
-#     validates :subject, presence: true 
-#     validates :description, presence: true 
-#     validates :type, presence: true 
-#     validates :status, presence: true 
-#     validates :responsible, presence: true 
-#     validates :assignee, presence: true 
-#     validates :started_at, presence: true     
-#     validates :completed_at, presence: true                                 
-# end
+ class Task < ApplicationRecord    
+    belongs_to :customer, class_name:"Company" 
+    belongs_to :assignee, class_name:"User" 
+    belongs_to :requested_by, class_name:"User" 
+    belongs_to :responsible, class_name:"User" 
+    belongs_to :status, class_name:"TaskStatus" 
+    belongs_to :type, class_name:"TaskType" 
+    
+    validates :code, presence: true 
+    validates :customer, presence: true   
+    validates :assignee, presence: true 
+    validates :requested_by, presence: true   
+    validates :responsible, presence: true 
+    validates :status, presence: true
+    validates :type, presence: true 
+    validates :subject, presence: true 
+    validates :description, presence: true 
+    validates :started_at, presence: true     
+    validates :completed_at, presence: true                                 
+ end
