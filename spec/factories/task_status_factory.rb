f304e956ec3f82f35a6e71d8f34c24f7e0a7c8b6
BryanPeens <peensbryan1@gmail.com>
2020-08-07 01:15:47 +0200

    added changes to factory_bot setup and rspec setup

---
+++ spec/factories/task_status_factory.rb
@@ -0,0 +1,7 @@
+# This will guess the User class
+FactoryBot.define do
+    factory :task_status do
+      code { "new" }
+      title  { "new" }
+    end
+  end
---
+++ spec/models/task_status_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+RSpec.describe TaskStatus, type: :model do
+    it "description" do
+        expect(true).to equal(true)
+    end
+end
